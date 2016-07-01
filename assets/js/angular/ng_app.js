3d5ebb751a11286eeb963347b3c1a59436480fbf
mason.zhang <mason.zhang@trantect.com>
2016-07-01 18:00:44 +0800

    rm no used file

--- assets/js/angular/ng_app.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('inputInvalid', []);
-app.controller('validateCtrl', function($scope) {
-    // $scope.username = 'John Doe';
-    // $scope.email = 'john.doe@gmail.com';
-});
